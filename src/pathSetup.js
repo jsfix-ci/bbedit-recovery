6e41acd920e01ff9bab5546bb33b6891e79668dd
codingChewie <codingChewie@gmail.com>
2021-07-23 13:00:29 -0500

    path setup

--- src/pathSetup.js
+++ src/pathSetup.js
@@ -1,9 +1,15 @@
+fs = require('fs')
 path = require('path')
 const { homedir } = require('os')
+const chalk = require('chalk')
 
 module.exports = p => {
-  const sp = p.trim()
-
-  if (sp.toLowerCase() === 'desktop') return path.join(homedir(), 'Desktop')
-  if (sp.toLowerCase().match(/(docs|documents)/)) return path.join(homedir(), 'Documents')
+  try {
+    if (p.toLowerCase() === 'desktop') return path.join(homedir(), 'Desktop')
+    if (p.toLowerCase().match(/(docs|documents)/)) return path.join(homedir(), 'Documents')
+    return fs.lstatSync(p).isDirectory() === true ? p : new Error()
+  } catch {
+    console.log(chalk.red('Error:'), chalk.white(`Path is not a valid directory`))
+    process.exit(1)
+  }
 }
