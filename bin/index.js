185ce75b4e29d9e29f86400a1d602633952aa45c
codingChewie <codingChewie@gmail.com>
2021-07-24 14:15:57 -0500

    removed trycatch

--- bin/index.js
+++ bin/index.js
@@ -6,25 +6,20 @@ const itemsBB = require('../src/itemsBB')
 const copyBB = require('../src/copyBB')
 
 module.exports = async (dest, name, cli = false) => {
-  try {
-    const obj = {
-      input: await findBB(),
-      output: await pathSetup(dest),
-      name,
-    }
-    obj.items = await itemsBB(obj.input)
-
-    await copyBB(obj)
-
-    cli === true
-      ? console.log(
-          chalk.blue(`BBEdit Recovery Completed`),
-          chalk.green(`\nFILES: `),
-          chalk.white(`${obj.output}/${obj.name}\n`),
-        )
-      : ''
-  } catch {
-    console.log(chalk.red('Error:'), chalk.white(`Ran into run error`))
-    process.exit(1)
+  const obj = {
+    input: await findBB(),
+    output: await pathSetup(dest),
+    name,
   }
+  obj.items = await itemsBB(obj.input)
+
+  await copyBB(obj)
+
+  cli === true
+    ? console.log(
+        chalk.blue(`BBEdit Recovery Completed`),
+        chalk.green(`\nFILES: `),
+        chalk.white(`${obj.output}/${obj.name}\n`),
+      )
+    : ''
 }
