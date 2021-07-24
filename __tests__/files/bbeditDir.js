3d75457291e99bf41211669d5fb7c038deb9f370
codingChewie <codingChewie@gmail.com>
2021-07-24 13:07:43 -0500

    added test files

---
+++ __tests__/files/bbeditDir.js
@@ -0,0 +1,12 @@
+exports.bbArray = [
+  'Attachment Scripts',
+  'Clippings',
+  'Completion Data',
+  'Custom Keywords',
+  'HTML Templates',
+  'Language Modules',
+  'Menu Scripts',
+  'Packages',
+  'Preview CSS',
+  'Preview Templates',
+]
