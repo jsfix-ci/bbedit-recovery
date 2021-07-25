edefe3971929e66b4dcde974d43170197ae32a7b
codingChewie <codingChewie@gmail.com>
2021-07-24 19:06:13 -0500

    added ability to pass BBEdit path

--- src/findBB.js
+++ src/findBB.js
@@ -3,9 +3,9 @@ path = require('path')
 const { homedir } = require('os')
 const chalk = require('chalk')
 
-module.exports = () => {
+module.exports = proPath => {
   try {
-    const bbPath = path.join(homedir(), 'Library', 'Application Support', 'BBEdit')
+    const bbPath = proPath || path.join(homedir(), 'Library', 'Application Support', 'BBEdit')
     return fs.lstatSync(bbPath).isDirectory() === true ? bbPath : new Error()
   } catch {
     console.log(chalk.red('Error:'), chalk.white(`BBEdit not found in Application Support`))
